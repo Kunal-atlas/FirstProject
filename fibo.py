e0406b3dc387e3be9422b476c7676255740f49eb
Kunal Meher <kunal.meher@atlasuniversity.edu.in>
2024-11-22 11:51:40 +0530

    15th Commit.

--- fibo.py
+++ fibo.py
@@ -1,14 +1,4 @@
-def fibonacci():
-    a,b=0,1
-    #print(a,b)
-    n=int(input("Enter the Length = "))
-    for i in range(n):
-        c=a+b
-        a=b
-        b=c
-        print(c)
 print("Fibonacci Series is as follows: ")
-fibonacci()
 print("Done Successfully.")
 print("Bye Bye, See you.")
 print("Good Gay!!")
